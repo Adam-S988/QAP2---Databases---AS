b4e23d504f7e95181ce8c1433de82bf1a9e6cd19
Adam-S988 <adam.stevenson@keyin.com>
2024-10-09 11:10:12 -0230

    Inserted the data into the tables

--- Problem1.sql
+++ Problem1.sql
@@ -5,3 +5,30 @@ CREATE TABLE professors (professor_id SERIAL PRIMARY KEY, first_name TEXT, last_
 CREATE TABLE courses (course_id SERIAL PRIMARY KEY, course_name TEXT, course_description TEXT, professor_id INT REFERENCES professors(professor_id))
 
 CREATE TABLE enrollments (student_id INT REFERENCES students(student_id), course_id INT REFERENCES courses(course_id), enrollment_date DATE, PRIMARY KEY (student_id, course_id))
+
+-- Inserting the Data
+INSERT INTO students(first_name, last_name, email, school_enrollment_date)
+VALUES('Jane', 'Fonda', 'j.fonda24@gmail.com', 'September 3 2022'),
+('Ryan', 'Reynolds', 'deadpool42@gmail.com', 'September 5 2024'),
+('Bruce', 'Wayne', 'bat_lover33@gmail.com', 'May 25 2007'),
+('Maggie', 'Smith', 'ladyviolet@gmail.com', 'September 1 2019'),
+('Valerie', 'Frizzle', 'magicschoolbus@gmail.com', 'June 15 2017');
+
+INSERT INTO professors(first_name, last_name, department)
+VALUES('Charlie', 'Chaplin', 'Theatre Arts'),
+('Gwen', 'Stefani', 'Music'),
+('Bill', 'Nye', 'Science'),
+('Edgar', 'Poe', 'English');
+
+INSERT INTO courses(course_name, course_description, professor_id)
+VALUES('Intro to Music Theory', '"Intro to Music Theory" explores the fundamental concepts of music, including notation, harmony, rhythm, and melody, empowering students to understand and create their own musical compositions.', 2),
+('Shakespeare', '"Shakespeare" delves into the life, works, and enduring influence of William Shakespeare, examining his plays and poetry through historical, cultural, and literary lenses.', 4),
+('Fundimentals of Rudimentary Chemistry', '"Fundamentals of Rudimentary Chemistry" introduces essential concepts of chemistry, including atomic structure, chemical reactions, and basic laboratory techniques, laying the groundwork for further exploration in the field.', 3);
+
+INSERT INTO enrollments(student_id, course_id, enrollment_date)
+VALUES (1, 3, 'September 6, 2022'),
+(1, 2, 'September 6, 2022'),
+(2, 2, 'September 7, 2024'),
+(3, 1, 'June 3, 2007'),
+(3, 3, 'September 2, 2007'),
+(4, 2, 'September 5, 2019');
